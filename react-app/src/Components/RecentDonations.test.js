78842d32a5d774d63478486a45949deeeda87b88
alejo4373 <alejandrofranco@pursuit.org>
2020-04-18 02:44:29 -0400

    test RecentDonations

---
+++ react-app/src/Components/RecentDonations.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import RecentDonations from './RecentDonations';
+
+const sampleDonations = [
+  {
+    name: 'John',
+    caption: 'Here take a break from work',
+    amount: 30
+  },
+  {
+    name: 'Emily',
+    caption: '',
+    amount: 110
+  },
+  {
+    name: 'Sam',
+    caption: 'Go to miami',
+    amount: 30
+  }
+]
+
+describe('RecentDonations', () => {
+  it('Displays a list of donation cards', () => {
+    const { container, getAllByText } = render(<RecentDonations donations={sampleDonations} />)
+    expect(getAllByText(/donated/).length).toBe(sampleDonations.length)
+    expect(container).toMatchSnapshot()
+  })
+})
