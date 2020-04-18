cbd188cb06d11caf41a7749e83eaac4864222b79
alejo4373 <alejandrofranco@pursuit.org>
2020-04-18 04:15:44 -0400

    test form component

--- react-app/src/Components/Form.jsx
+++ react-app/src/Components/Form.jsx
@@ -11,22 +11,44 @@ const Form = (props) => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} data-testid="form">
       <div className="form-group">
         <label htmlFor='nameInput'>Name</label>
-        <input type="text" className="form-control" id="nameInput" value={formDonor} onChange={handleInputChange} />
+        <input
+          type="text"
+          placeholder="Jon Doe"
+          className="form-control"
+          id="nameInput"
+          value={formDonor}
+          onChange={handleInputChange}
+        />
       </div>
 
       <div className="form-group">
         <label htmlFor='captionInput'>Caption</label>
-        <input type="text" className="form-control" id="captionInput" value={formCaption} onChange={handleInputChange} />
+        <input
+          type="text"
+          placeholder="Good luck"
+          className="form-control"
+          id="captionInput"
+          value={formCaption}
+          onChange={handleInputChange}
+        />
       </div>
 
       <div className="form-group">
         <label htmlFor='amountInput'>Amount to Donate</label>
-        <input type="range" className="custom-range" id="amountInput" min="5" max="1000" value={formAmount} onChange={handleInputChange} />
+        <input
+          type="range"
+          className="custom-range"
+          id="amountInput"
+          min="5"
+          max="1000"
+          value={formAmount}
+          onChange={handleInputChange}
+        />
         <blockquote className="blockquote text-right">
-          <p className="h3 mb-0">$ {formAmount}</p>
+          <p className="h3 mb-0">${formAmount}</p>
           <button type="submit" className="btn btn-lg btn-success my-4">Donate</button>
         </blockquote>
       </div>
---
+++ react-app/src/Components/Form.test.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { render, fireEvent, getByDisplayValue } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Form from './Form';
+
+describe('Form', () => {
+  it('Displays four input controls: Name, Caption, ammount slider and submit button', () => {
+    const { getByPlaceholderText, getByRole, getByText } = render(<Form />)
+    const nameInput = getByPlaceholderText('Jon Doe')
+    const captionInput = getByPlaceholderText('Good luck')
+    const slider = getByRole('slider')
+    const donateButton = getByText('Donate')
+    expect(nameInput).toBeInTheDocument()
+    expect(captionInput).toBeInTheDocument()
+    expect(slider).toBeInTheDocument()
+    expect(donateButton).toBeInTheDocument()
+  })
+
+  it('Display info that is passed as props', () => {
+    const formDonor = "Sponge Bob"
+    const formCaption = "Come down here"
+    const formAmount = 350
+    const { getByDisplayValue, getByText } = render(
+      <Form formDonor={formDonor} formCaption={formCaption} formAmount={formAmount} />
+    )
+
+    const nameInput = getByDisplayValue(formDonor)
+    const captionInput = getByDisplayValue(formCaption)
+    const amountDisplay = getByText(`$${formAmount}`)
+    expect(nameInput).toBeInTheDocument()
+    expect(captionInput).toBeInTheDocument()
+    expect(amountDisplay).toBeInTheDocument()
+
+  })
+
+  it('Invokes handleFormInput when typing or editing a value', () => {
+    const formDonor = "Sponge"
+    const formCaption = "Come down"
+    const formAmount = 350
+    const handleFormInput = jest.fn()
+    const { getByRole, getByDisplayValue } = render(
+      <Form
+        formDonor={formDonor}
+        formCaption={formCaption}
+        formAmount={formAmount}
+        handleFormInput={handleFormInput}
+      />
+    )
+    const nameInput = getByDisplayValue(formDonor)
+    const captionInput = getByDisplayValue(formCaption)
+    const slider = getByRole('slider')
+
+    userEvent.type(nameInput, " Bob")
+    expect(handleFormInput).toHaveBeenLastCalledWith("nameInput", " Bob")
+
+    userEvent.type(captionInput, " here")
+    expect(handleFormInput).toHaveBeenLastCalledWith("captionInput", " here")
+
+    fireEvent.change(slider, { target: { value: 500 } })
+    expect(handleFormInput).toHaveBeenLastCalledWith("amountInput", "500")
+  })
+
+  it('Invokes handleSubmit when submitting form', () => {
+    const handleSubmit = jest.fn()
+    const { getByTestId } = render(<Form handleSubmit={handleSubmit} />)
+
+    const form = getByTestId('form')
+    fireEvent.submit(form)
+    expect(handleSubmit).toHaveBeenCalled()
+  })
+
+  // Can't be done here because requires state and state is in the parent component (App)
+  // it.skip('Inputs get cleared on submit', () => { })
+
+  // Can't be done here because requires state and state is in the parent component (App)
+  // it.skip('Values entered in form displays in respective places', () => { })
+
+})
