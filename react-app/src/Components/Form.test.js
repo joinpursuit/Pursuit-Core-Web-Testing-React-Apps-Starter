0b8e9afb2490c29c389b2d262f831e77ce43356a
alejo4373 <alejandrofranco@pursuit.org>
2020-04-18 04:22:06 -0400

    remove unused import

--- react-app/src/Components/Form.test.js
+++ react-app/src/Components/Form.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, getByDisplayValue } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Form from './Form';
 
