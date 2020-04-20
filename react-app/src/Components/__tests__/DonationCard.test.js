33c27cdf15f2070bb6ef91763b53bb13d60897ae
alejo4373 <alejandrofranco@pursuit.org>
2020-04-20 12:59:41 -0400

    annotate DonationCard.test with comments and explicit query & expectation

--- react-app/src/Components/__tests__/DonationCard.test.js
+++ react-app/src/Components/__tests__/DonationCard.test.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import DonationCard from '../DonationCard';
 
-describe('DonationCard Component', () => {
-  test('Display Donor name, message and ammount', () => {
-    render(<DonationCard name="Alejo" message="Good Luck" amount="100" />)
-    expect(screen.getByText("Alejo donated $100")).toBeInTheDocument()
-    expect(screen.getByText("Good Luck")).toBeInTheDocument()
-  })
+test('DonorCard displays donor name, message and ammount', () => {
+
+  // Render the component
+  render(<DonationCard name="Alejo" message="Good Luck" amount="100" />)
+
+  // Get elements by the expected text and expect them to be displaying
+  const donationHeading = screen.getByText("Alejo donated $100")
+  const donationMessage = screen.getByText("Good Luck")
+
+  expect(donationHeading).toBeInTheDocument()
+  expect(donationMessage).toBeInTheDocument()
 })
