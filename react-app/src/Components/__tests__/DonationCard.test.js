17411c0a5e6345da6d838bf0aeef2ea2545b706e
alejo4373 <alejandrofranco@pursuit.org>
2020-04-20 12:29:40 -0400

    use screen for DonationCard.test

--- react-app/src/Components/__tests__/DonationCard.test.js
+++ react-app/src/Components/__tests__/DonationCard.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import DonationCard from '../DonationCard';
 
 describe('DonationCard Component', () => {
   test('Display Donor name, message and ammount', () => {
-    const { getByText } = render(<DonationCard name="Alejo" message="Good Luck" amount="100" />)
-    expect(getByText(/Alejo donated \$100/)).toBeInTheDocument()
-    expect(getByText("Good Luck")).toBeInTheDocument()
+    render(<DonationCard name="Alejo" message="Good Luck" amount="100" />)
+    expect(screen.getByText("Alejo donated $100")).toBeInTheDocument()
+    expect(screen.getByText("Good Luck")).toBeInTheDocument()
   })
 })
